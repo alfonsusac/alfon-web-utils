b1aaf3cb7fc3a55315a8e0018fbfe6fcbb24236c
Alfonsus Ardani <alfonsusac@gmail.com>
2025-04-04 15:57:08 +0700

    change error outupt

--- src/app/api/get-context-length/route.ts
+++ src/app/api/get-context-length/route.ts
@@ -1,6 +1,7 @@
 import type { NextRequest } from "next/server";
 import { withLimit } from "@/lib/ratelimit";
 import { getStringContextLength } from "./lib";
+import { error } from "console";
 
 export const config = { runtime: "edge" };
 
@@ -8,25 +9,25 @@ export const config = { runtime: "edge" };
 export async function GET(request: NextRequest) {
   try {
     return await withLimit(request, "1 per 3 seconds", async (user, limit) => {
-      // CACHE THIS
       try {
-        const length = await getStringContextLength(
-          // content
-        )
+        const length = await getStringContextLength()
         return Response.json({
-          data: {
-            length,
-          },
+          data: { length },
           limit
         })
       } catch (error) {
         console.log(error)
-        return new Response(`Failed getting context string legnth (${error instanceof Error ? error.message : JSON.stringify(error)})`, { status: 500 })
+        return Response.json({
+          error: `Failed getting context string legnth (${ error instanceof Error ? error.message : JSON.stringify(error) })`
+        })
       }
 
     })
   } catch {
-    return new Response("Internal Server Error (route.ts)", { status: 500 })
+    return Response.json({
+      error: "Failed procesing GET request",
+      status: 500
+    })
   }
 }
 
