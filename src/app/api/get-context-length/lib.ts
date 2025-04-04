4ed4a89e5e72c9b99c754c772726312647827342
Alfonsus Ardani <alfonsusac@gmail.com>
2025-04-04 15:43:20 +0700

    try again?

--- src/app/api/get-context-length/lib.ts
+++ src/app/api/get-context-length/lib.ts
@@ -1,44 +1,12 @@
-// import wasm from "tiktoken/lite/tiktoken_bg.wasm?module";
-// import model from "tiktoken/encoders/cl100k_base.json";
-// import { init, Tiktoken } from "tiktoken/lite/init";
 
 import { get_encoding } from "tiktoken";
-
-// import type { Tiktoken, TiktokenBPE } from "tiktoken"
-
-// export const config = { runtime: "edge" };
-
-// import { init } from "tiktoken/init";
-// import { get_encoding } from "tiktoken/tiktoken_bg.wasm";
 
 export async function getStringContextLength(
   // str: string,
   // model: TiktokenModel = "gpt-4o"
 ) {
-  const encoding = get_encoding("cl100k_base");
+  const encoding = get_encoding("o200k_base");
   const tokens = encoding.encode("hello world");
   encoding.free();
   return tokens.length
-  //---
-  // try {
-  //   const { get_encoding } = await import("tiktoken");
-  //   const encoding = get_encoding("cl100k_base");
-  //   const tokens = encoding.encode(str);
-  //   encoding.free();
-  //   return tokens.length
-  // } catch (error) {
-  //   throw error
-  // }
-  // await init((imports) => WebAssembly.instantiate(wasm, imports));
-
-  // const encoding = new Tiktoken(
-  //   model.bpe_ranks,
-  //   model.special_tokens,
-  //   model.pat_str
-  // );
-
-  //   const tokens = encoding.encode(str);
-  //   encoding.free();
-  //   return tokens.length
-
 }
