21c4dc27fb26777b99738d327e10fc660b80b1be
Alfonsus Ardani <alfonsusac@gmail.com>
2025-04-05 14:18:05 +0700

    using unstablecache

--- src/pages/api/tiktoken/tokenize.ts
+++ src/pages/api/tiktoken/tokenize.ts
@@ -4,6 +4,7 @@ import JSONmodelToEncoding from 'tiktoken/model_to_encoding.json'
 import JSONregistry from 'tiktoken/registry.json'
 import { rateLimiter } from "@/lib/ratelimit";
 import { getUserPagesDir } from "@/lib/user";
+import { unstable_cache } from "next/cache";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
@@ -16,36 +17,25 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       reset: rl.reset,
     })
   }
-
-
   // Validate method
   if (req.method !== "GET" && req.method !== "POST") return res.status(405).json({ error: "Method not allowed" });
-
   // Get body
   const body: unknown = req.method === "POST" ? req.body : req.query;
-
   // Validate body
   if (
     !body
     || typeof body !== "object"
     || 'content' in body === false
     || typeof body.content !== "string"
-  ) {
-    return res.status(400).json({ error: "Invalid request body" });
-  }
+  ) return res.status(400).json({ error: "Invalid request body" });
 
-  // let encoding: typeof tiktokenEncodings[number] | undefined = undefined;
-  // let model: typeof tiktokenModels[number] | undefined = undefined;
 
   if (
     'encoding' in body
     && typeof body.encoding === 'string'
   ) {
     if (isTiktokenEncoding(body.encoding)) {
-      console.log("tiktoken encoding")
-      const encoder = get_encoding(body.encoding);
-      const tokens = encoder.encode(body.content);
-      encoder.free();
+      const tokens = await cached_encodeFromEncodings(body.encoding, body.content)
       return res.status(200).json({
         length: tokens.length,
         remaining: rl.remaining,
@@ -61,10 +51,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     && typeof body.model === 'string'
   ) {
     if (isTiktokenModel(body.model)) {
-      console.log("tiktoken model")
-      const encoder = encoding_for_model(body.model);
-      const tokens = encoder.encode(body.content);
-      encoder.free();
+      const tokens = await cached_encodeFromModels(body.model, body.content)
       return res.status(200).json({
         length: tokens.length,
         remaining: rl.remaining,
@@ -94,9 +81,25 @@ type TiktokenEncodings = Array<keyof typeof JSONregistry>[number]
 function isTiktokenEncoding(encoding: string): encoding is TiktokenEncodings {
   return tiktokenEncodings.includes(encoding);
 }
+function encodeFromEncodings(encoding: TiktokenEncodings, content: string) {
+  const encoder = get_encoding(encoding);
+  const tokens = encoder.encode(content);
+  encoder.free();
+  return tokens;
+}
+const cached_encodeFromEncodings = unstable_cache(async (...args: Parameters<typeof encodeFromEncodings>) => encodeFromEncodings(...args), undefined)
+
+
 
 export const tiktokenModels = Object.keys(JSONmodelToEncoding)
 type TiktokenModels = Array<keyof typeof JSONmodelToEncoding>[number]
 function isTiktokenModel(model: string): model is TiktokenModels {
   return tiktokenModels.includes(model);
 }
+function encodeFromModels(model: TiktokenModels, content: string) {
+  const encoder = encoding_for_model(model);
+  const tokens = encoder.encode(content);
+  encoder.free();
+  return tokens;
+}
+const cached_encodeFromModels = unstable_cache(async (...args: Parameters<typeof encodeFromModels>) => encodeFromModels(...args), undefined)
